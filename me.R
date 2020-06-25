3f8f030fa76539b929790e4a4cfade5960459c13
hufeide <704725096@qq.com>
2020-06-25 10:35:38 +0800

    fix

---
+++ me.R
@@ -0,0 +1,7 @@
+# git remote add origin  https://github.com/hufeide/myfirst
+# git config remote.origin.url git@github.com:hufeide/myfirst
+# git pull  origin master
+# git push  origin master
+a = 1
+
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcf77eb2tAcl5ehYrk9A/CPLVJlXrabjIod1oMh0CFXCK9iakKQ0tJvFRJ6EIPEUf2coU/BMlSyzKPDn+eLChzS10yvAi6ms+phwOZHzelVx8Zq6szYhbD08fRZ4fygYCr2eRLyNLemliF8xV683lLFHJv7UBfGORbEbBga6cqyWekTw1hbnZYfMgoqboTF33pUESG1C6HWWF/5PKvyXlNewfxaBaRBc88UiBmS0UOpDkAtMrSTgkQ3IgXySwAjLIDJJO7UVFsgFYl9Gb3NkRrdjppMWat6MyzHI2hhCcikjNs++6Pozdz3D3Lc5vNMkwYszM0ZbNSkE6A/v6Feh+l dell@DESKTOP-BTCTCI8
